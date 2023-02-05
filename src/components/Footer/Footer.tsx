9cb3a98296e5d4633f766eba68aa025ea7a8a5b7
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-05 14:00:23 +0100

    Add Twitter share link

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,8 @@ import { getCurrentYear } from "../../utils/generic";
 
 const Footer = () => {
   const currentYear = getCurrentYear();
-  const twitterLink = "#!"; // TODO
+  const twitterLink =
+    "https://twitter.com/intent/tweet?text=Check%20out%20%23LoopGate%2C%20a%20%23Dapp%20that%20facilitates%20Token-Gated%20Content%20using%20%23Loopring%20%23L2%20%23NFTs%20%F0%9F%91%80%20%F0%9F%A4%AF&url=https%3A%2F%2Floopgate.netlify.app%2F&via=0xGeel";
   const sourceCodeLink = "https://github.com/0xGeel/loopring-token-gating";
 
   return (
