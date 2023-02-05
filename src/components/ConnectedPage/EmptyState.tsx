53ab1464b3d112b16833554f03edef7b0d49d5f3
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-05 15:39:10 +0100

    Add style to empty state

--- src/components/ConnectedPage/EmptyState.tsx
+++ src/components/ConnectedPage/EmptyState.tsx
@@ -9,7 +9,9 @@ const EmptyState = () => {
     <div className="px-6 py-8 border-2 border-dashed border-white/20 rounded-md flex flex-col items-center justify-center space-y-8 backdrop-blur-xl bg-slate-900/10 shadow-xl shadow-slate-900/50">
       <EyeSlashIcon className="w-16 h-16 text-sky-500/40" />
       <div className="flex flex-col items-center">
-        <h1 className="text-lg font-medium">There&apos;s nothing here!</h1>
+        <h1 className="text-xl font-medium font-display mb-4">
+          There&apos;s nothing here!
+        </h1>
         <p className="text-white/70 text-center max-w-sm">
           Bummer.. your wallet does not hold any NFTs that provide access to
           token-gated content.
