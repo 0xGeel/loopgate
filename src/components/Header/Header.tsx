3f0ca1a7f973a46ef10f5f2cc0815f9e54bfb803
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-05 12:27:46 +0100

    Remove unused attr

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,7 @@ import LogoBrandLight from "@/public/images/logo/loopgate-brand-light.svg";
 const Header = () => {
   return (
     <div className="w-full flex py-3 px-4 sm:px-6 sticky top-0 items-center justify-between border-b sm:border-b-0 border-white/10 bg-slate-900/50 backdrop-blur-lg z-10">
-      <Image
-        src={LogoBrandLight}
-        alt="loopgate Logo"
-        // width={108}
-        height={32}
-      />
+      <Image src={LogoBrandLight} alt="loopgate Logo" height={32} />
 
       <ConnectKitButton />
     </div>
