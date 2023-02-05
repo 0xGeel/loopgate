a854e79de4d6d04e505bc4d627015c5da6aea335
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-05 14:00:17 +0100

    Enhancement: Add prompt for connected but unverified users

--- src/components/ConnectPrompt/ConnectPrompt.tsx
+++ src/components/ConnectPrompt/ConnectPrompt.tsx
@@ -1,6 +1,18 @@
 import { ConnectKitButton } from "connectkit";
+import { useAccount } from "wagmi";
+
+const SignInHint = () => (
+  <div className="absolute flex flex-col items-center w-full">
+    <div className="w-3 h-3 rounded-tl-sm bg-[#38BDF8] rotate-45 -mb-2 mt-2"></div>
+    <p className="text-xs text-center bg-[#38BDF8] rounded-md text-black px-2 py-1.5">
+      Sign in to continue
+    </p>
+  </div>
+);
 
 const ConnectPrompt = () => {
+  const { address } = useAccount();
+
   return (
     <div className="flex flex-col items-center justify-center flex-grow space-y-10">
       <div className="space-y-2 flex flex-col items-center">
@@ -9,7 +21,14 @@ const ConnectPrompt = () => {
           To see if you are worthy of unlocking content...
         </p>
       </div>
-      <ConnectKitButton />
+      {address ? (
+        <div className="relative">
+          <ConnectKitButton />
+          <SignInHint />
+        </div>
+      ) : (
+        <ConnectKitButton />
+      )}
     </div>
   );
 };
