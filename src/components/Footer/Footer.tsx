9176ae6e51a77eebf712d311abe036eb3d985b6e
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-07 21:39:02 +0100

    Minor tweaks, update package

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
   const sourceCodeLink = "https://github.com/0xGeel/loopring-token-gating";
 
   return (
-    <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto ">
+    <div className="py-4 px-8 w-full flex flex-col md:flex-row items-center justify-between space-y-1 md:space-y-0 mt-auto text-center">
       <p className="text-white/40">
         {currentYear},{" "}
         <FooterLink href="https://twitter.com/0xgeel">geel.eth</FooterLink> — 
@@ -17,11 +17,11 @@ const Footer = () => {
       </p>
       <div className="space-x-4">
         <FooterLink href={sourceCodeLink}>
-          <p>Source Code</p>
+          <p className="hidden md:inline-block">Source Code</p>
           <GithubIcon className="w-5 h-5" />
         </FooterLink>
         <FooterLink href={twitterLink}>
-          <p>Share on Twitter</p>
+          <p className="hidden md:inline-block"> Share on Twitter</p>
           <TwitterIcon className="w-5 h-5" />
         </FooterLink>
       </div>
