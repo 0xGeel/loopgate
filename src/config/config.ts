00a1365b3eb8fcc277539e1a74fbe0a9d5be32e1
0xGeel <gallus.septimius.cicero@gmail.com>
2023-02-11 16:04:35 +0100

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,8 +1,24 @@
-const PINATA_GATEWAY_URL = "j.mypinata.cloud";
-
 import { Unlockable } from "./types";
 
-// Specify unlockable files on IPFS by specifying their Content Identifier (CID), and unlock criteria.
+/*********************************************************
+Replace the `unlockables` below with your own content. 
+Here are two examples:
+
+1️⃣ Unlock a Submarined file by owning one specific NFT.
+{ 
+  cid: "<YOUR_CID>", 
+  nftId: ["<YOUR_NFT_ID"]
+}
+
+2️⃣ Unlock a Submarined file by owning multiple NFTs.
+{
+  cid: "<YOUR_CID",
+  nftId: [
+    "<YOUR_FIRST_NFT_ID", "<YOUR_SECOND_NFT_ID", 
+  ]
+}
+*********************************************************/
+
 const unlockables: Unlockable[] = [
   {
     cid: "bafybeiehgpaip4f7jafzf7imgannx3nnv3ubaiwp6ph56mlyzijpqxi45m", // HTML blog example
@@ -25,4 +41,4 @@ const unlockables: Unlockable[] = [
   },
 ];
 
-export { unlockables, PINATA_GATEWAY_URL };
+export { unlockables };
