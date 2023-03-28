4d20f43ad77290e580c70314b3adc4f5d3ddbdd9
0xGeel <gallus.septimius.cicero@gmail.com>
2023-03-28 20:34:28 +0200

    Add Snazzy pattern to bg

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full flex py-3 px-4 sm:px-6 sticky top-0 items-center justify-between border-b sm:border-b-0 border-white/10 bg-slate-900/50 backdrop-blur-lg z-10">
+    <div className="w-full flex py-3 px-4 sm:px-6 sticky top-0 items-center justify-between border-b sm:border-b-0 border-white/10 bg-slate-900/50 backdrop-blur-lg md:bg-transparent md:backdrop-blur-0 z-10">
       <Link href="/">
         <Image src={LogoBrandLight} alt="loopgate Logo" height={32} />
       </Link>
